916e53a3a4dbae5232ccc4a5af35e6a03c776363
Priyanka Shah <priyanka2003shah@gmail.com>
2022-04-27 15:24:11 -0400

    addEventListeners added

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ import start from "/index.js";
 
 let ttt = new start();
 let gamePlay = new play(document.getElementById("app"));
+
+gamePlay.onBoard = function(i){
+console.log('tile clicked: ${i}');
+}
+gamePlay.refreshClick = function(){
+    console.log("Play Again");
+}
--- playing.js
+++ playing.js
@@ -13,7 +13,33 @@ export default class play{
             <i class="icon">play again</i>
 
         </button>
+        <div class = "gameBoard">
+        <div class = "boxes" data-index= "0" >O</div>
+        <div class = "boxes" data-index= "1" >O</div>
+        <div class = "boxes" data-index= "2" >O</div>
+        <div class = "boxes" data-index= "3" >O</div>
+        <div class = "boxes" data-index= "4" >O</div>
+        <div class = "boxes" data-index= "5" >O</div>
+        <div class = "boxes" data-index= "6" >O</div>
+        <div class = "boxes" data-index= "7" >O</div>
+        <div class = "boxes" data-index= "8" >O</div>
+        </div>
         </div>
         `;
+        this.onBoard = undefined;
+        this.refreshClick = undefined;
+        this.root.querySelectorAll(".boxes").forEach(tile => {
+            tile.addEventListener("click", () =>{
+                if (this.onBoard) {
+                    this.onBoard(tile.dataset.index);
+                } 
+            });
+           
+        });
+        this.root.querySelector(".restart").addEventListener("click", () => {
+            if (this.refreshClick) {
+                this.refreshClick();
+            }
+        });
     }
 }
